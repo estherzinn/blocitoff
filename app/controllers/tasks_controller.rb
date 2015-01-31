c24ef69741ab7d3b4e314bcfa12d60ee1ebbde8a
Esther Zinn <Bionicfemme@me.com>
2015-01-31 18:21:50 -0500

    Added a user id to tasks, configured user task associations

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+  before_action :authenticate_user!
+
+  respond_to :html
+ 
+def index
+  @tasks = current_user.tasks
+  respond_with(@tasks)
+end
+ 
+def create
+  @task = current_user.tasks.new(task_params)
+  @task.save
+  respond_with(@task)
+end
+
+  
+
+  
+
+  def show
+    respond_with(@task)
+  end
+
+  def new
+    @task = Task.new
+    respond_with(@task)
+  end
+
+  def edit
+  end
+
+ 
+
+  def update
+    @task.update(task_params)
+    respond_with(@task)
+  end
+
+  def destroy
+    @task.destroy
+    respond_with(@task)
+  end
+
+  private
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:content)
+    end
+end
